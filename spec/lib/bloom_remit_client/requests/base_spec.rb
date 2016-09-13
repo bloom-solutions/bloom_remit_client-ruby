3dc68011c07a929564475b68ac5cb5a6735c2ac8
Ramon Tayag <ramon.tayag@gmail.com>
2016-09-13 10:13:54 +0800

    Fix base request spec

--- spec/lib/bloom_remit_client/requests/base_spec.rb
+++ spec/lib/bloom_remit_client/requests/base_spec.rb
@@ -1,36 +1,20 @@
 require 'spec_helper'
 
 module BloomRemitClient
-  RSpec.describe Base, type: %i[model virtus] do
+  module Requests
+    RSpec.describe Base, type: %i[model virtus] do
 
-    describe "attributes" do
-      subject { described_class }
-      it { is_expected.to have_attribute(:token, String) }
-      it { is_expected.to have_attribute(:secret, String) }
-      it { is_expected.to have_attribute(:url, String) }
-      it { is_expected.to have_attribute(:agent_id, String) }
-      it do
-        is_expected.to have_attribute(:path, String).with_default(:default_path)
+      describe "attributes" do
+        subject { described_class }
+        it { is_expected.to have_attribute(:api_token, String) }
+        it { is_expected.to have_attribute(:api_secret, String) }
       end
-      it do
-        is_expected.to have_attribute(:endpoint, String).
-          with_default(:default_endpoint)
+
+      describe "validations" do
+        it { is_expected.to validate_presence_of(:api_token) }
+        it { is_expected.to validate_presence_of(:api_secret) }
       end
-    end
 
-    describe "validations" do
-      it { is_expected.to validate_presence_of(:token) }
-      it { is_expected.to validate_presence_of(:secret) }
-      it { is_expected.to validate_presence_of(:url) }
-      it { is_expected.to validate_presence_of(:path) }
     end
-
-    describe "#endpoint" do
-      it "defaults to url + path" do
-        request = described_class.new(url: "https://hello.com", path: "/path")
-        expect(request.endpoint).to eq "https://hello.com/path"
-      end
-    end
-
   end
 end
