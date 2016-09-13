ad3c7e3c79db02a7fcd8016001a036819de258ea
Ramon Tayag <ramon.tayag@gmail.com>
2016-09-13 11:19:39 +0800

    Set sandbox to true for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,9 @@ SPEC_DIR = Pathname.new(File.dirname(__FILE__))
 Dir[SPEC_DIR.join("support", "**", "*.rb")].each {|f| require f}
 
 CONFIG = YAML.load_file(SPEC_DIR.join("config.yml")).with_indifferent_access
+
+RSpec.configure do |c|
+  c.before do
+    BloomRemitClient.sandbox = true
+  end
+end
